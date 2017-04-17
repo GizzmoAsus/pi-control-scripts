bf97f340e384295a800918b6deedf37114681b79
Matthew Kellett <GizzmoAsus@gmail.com>
2017-04-17 14:34:13 +0800

    Fixed command to retrieve the windowid

--- switchtabs.sh
+++ switchtabs.sh
@@ -3,6 +3,6 @@
 # This script will switch between 2 tabs in Chromium
 
 export DISPLAY=":0"
-=$(ndowidxdotool search --onlyvisible --class chromium | head -1)
+windowid=$(xdotool search --onlyvisible --class chromium | head -1)
 xdotool windowactivate ${windowid}
 xdotool key ctrl+Tab
